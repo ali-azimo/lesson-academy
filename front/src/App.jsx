60c5d81d79117946cb2a42194b0f7d94c12df4f8
Ali Azimo <webdesign.aejl@gmail.com>
2025-07-01 23:41:26 +0200

    pages

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Cad from './pages/Cad';
+import Profile from './pages/Profile';
+import About from './pages/About';
+
 
 export default function App() {
-  return (
-    <div className='bg-red-500 h-screen w-screen'>App</div>
-  )
+  return  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cad" element={<Cad />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>; 
 }
